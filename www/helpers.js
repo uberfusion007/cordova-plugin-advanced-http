d902b300cf6d430be9e617b4700d07a4ab7d2183
HI-chehsing <chehsing.wong@hexainitiative.com>
2023-08-16 09:28:05 +0800

    Revert "[] Resolved capacitor issue when using formData"
    This reverts commit d79391b118a1588a869ad63cd86411a6a58efb98.

--- www/helpers.js
+++ www/helpers.js
@@ -459,9 +459,7 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
     }
 
     if (entry.value[1] instanceof global.Blob || entry.value[1] instanceof global.File) {
-      const fileReader = new global.FileReader();
-      const zoneOriginalInstance = fileReader["__zone_symbol__originalInstance"];
-      var reader = zoneOriginalInstance || fileReader;
+      var reader = new global.FileReader();
 
       reader.onload = function () {
         result.buffers.push(base64.fromArrayBuffer(reader.result));
